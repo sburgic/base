6ded30c3eb834ae395875103877c3a613f1e3b20
Sani Sasa Burgic <sani.sasa.burgic@gmail.com>
2020-03-22 18:44:03 +0100

    Parse comments in .dep files and fix directory handling

--- checkout.py
+++ checkout.py
@@ -2,10 +2,12 @@
 #    checkout.py
 #
 # Purpose
-#    Fetch all project dependencies requsted by input .def file.
+#    Fetch all project dependencies requested by input .def file.
 #
 # Revision Dates
 #    22-Mar-2020 (SSB) [] Initial
+#    22-Mar-2020 (BSE) [] Parse comments in .dep files
+#                         and fix directory handling
 
 import os
 import sys
@@ -19,8 +21,8 @@ except:
     import gdown
 
 # Define global variables
-libs_dir        = '../!libs/'
 gdrive_def_link = 'https://drive.google.com/uc?id='
+libs_dir_rel    = '../libs/'
 
 def dump_help():
     print('Usage example: checkout.py /etc/projects/jtag/jtag.dep')
@@ -41,41 +43,52 @@ def validate_args():
 def main():
     validate_args()
 
+    file_dir     = os.path.dirname( __file__ )
+    libs_dir     = os.path.join( file_dir, libs_dir_rel )
+    file_dir_abs = os.path.abspath( file_dir )
+    libs_dir_abs = os.path.abspath( libs_dir )
+
+    if ( False == os.path.exists( libs_dir_abs )):
+        os.system( 'mkdir ' + libs_dir_abs )
+
     try:
-        jfile = open('dependencies.json', 'r')
+        jfile = open(os.path.join( file_dir_abs,'dependencies.json'), 'r')
         jdata = json.load(jfile)
         jfile.close()
     except:
-        print('Error: File dependendies.json not accessible.')
+        print('Error: File dependencies.json not accessible.')
         sys.exit()
 
     try:
         ifile = open( sys.argv[1], 'r' )
         idata = ifile.readlines()
-        idata = [ x.strip() for x in idata ]
+        idata = [ x.partition("#")[0].strip() for x in idata ]
+        idata = filter(None, idata)
         ifile.close()
     except:
         print('Error: File ' + sys.argv[1] + ' not accessible.')
         dump_help()
 
     for lib in idata:
-        dep = next(( dep for dep in jdata if lib == dep["name"] ), False )
-        if ( False != dep ):
+        dep = next(( dep for dep in jdata if lib == dep["name"] ), None )
+        if ( None != dep ):
             if ( 'gdrive' == dep['location'] ):
-                if ( False == os.path.exists( libs_dir + lib )):
+                destination_folder = os.path.join( libs_dir_abs, lib )
+                destination_file   = destination_folder + '.zip'
+
+                if ( False == os.path.exists( destination_folder )):
                     gdown.download( gdrive_def_link + dep['link']
-                                  , libs_dir + lib + '.zip'
+                                  , destination_file
                                   , quiet = False
                                   )
                     print('Extracting files...')
-                    with zipfile.ZipFile( libs_dir + lib + '.zip'
-                                        , 'r' ) as zf:
-                        zf.extractall( libs_dir )
+                    with zipfile.ZipFile( destination_file, 'r' ) as zf:
+                        zf.extractall( libs_dir_abs )
                     print('Removing temporary files...')
-                    os.system('rm -rf ' + libs_dir + lib + '.zip')
+                    os.system('rm -rf ' + destination_file)
                 else:
                     print('Dependency ' + lib + ' already exists.')
-        else :
+        else:
             print('Dependency ' + lib  + ' not supported! Check the .dep file!')
 
 if __name__ == "__main__" :
