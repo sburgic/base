8ded9de315631e9169748491ee1bb2709c554984
Sani Sasa Burgic <sani.sasa.burgic@gmail.com>
2020-04-01 10:56:38 +0200

    Add oshelpers.mk and toolchain.mk

---
+++ oshelpers.mk
@@ -0,0 +1,25 @@
+## Name
+##   oshelpers.mk
+##
+## Purpose
+##   Host OS build definitions and functions
+##
+## Revision
+##    28-Mar-2020 (SSB) [] Initial
+
+ifeq ($(OS),Windows_NT)
+    HOST_OS := windows
+else
+    uname_s := $(shell uname -s)
+
+    ifeq ($(uname_s),Linux)
+        HOST_OS := linux
+    endif
+endif
+
+MD := mkdir -p
+RM := rm -rf
+
+# Convert lowercase to uppercase
+# We are using custom function to support all platforms
+toupper = $(subst a,A,$(subst b,B,$(subst c,D,$(subst d,D,$(subst e,E,$(subst f,F,$(subst g,G,$(subst h,H,$(subst i,I,$(subst j,J,$(subst k,K,$(subst l,L,$(subst m,M,$(subst n,N,$(subst o,O,$(subst p,P,$(subst q,Q,$(subst r,R,$(subst s,S,$(subst t,T,$(subst u,U,$(subst v,V,$(subst w,W,$(subst x,X,$(subst y,Y,$(subst z,Z,$1))))))))))))))))))))))))))
---
+++ toolchain.mk
@@ -0,0 +1,83 @@
+## Name
+##   toolchain.mk
+##
+## Purpose
+##   Toolchain definitions
+##
+## Revision
+##    30-Mar-2020 (SSB) [] Initial
+
+# Toolchain location
+TOOLCHAIN_ROOT    ?= C:/gcc-arm-none-eabi
+TARGET_TRIPLET    := arm-none-eabi
+TOOLCHAIN_BIN_DIR := $(TOOLCHAIN_ROOT)/bin
+TOOLCHAIN_INC_DIR := $(TOOLCHAIN_ROOT)/$(TARGET_TRIPLET)/include
+TOOLCHAIN_LIB_DIR := $(TOOLCHAIN_ROOT)/$(TARGET_TRIPLET)/lib
+
+# Tools location and definiton
+CC     := $(TOOLCHAIN_BIN_DIR)/$(TARGET_TRIPLET)-gcc
+LD     := $(TOOLCHAIN_BIN_DIR)/$(TARGET_TRIPLET)-ld
+AR     := $(TOOLCHAIN_BIN_DIR)/$(TARGET_TRIPLET)-ar
+AS     := $(TOOLCHAIN_BIN_DIR)/$(TARGET_TRIPLET)-as
+OBJCPY := $(TOOLCHAIN_BIN_DIR)/$(TARGET_TRIPLET)-objcopy
+OBJDMP := $(TOOLCHAIN_BIN_DIR)/$(TARGET_TRIPLET)-objdump
+SIZE   := $(TOOLCHAIN_BIN_DIR)/$(TARGET_TRIPLET)-size
+
+# Common flags for all tools
+TOOLCHAIN_COMMON_FLAGS := -mcpu=cortex-m4 \
+                          -mthumb \
+                          -mfloat-abi=hard \
+                          -mfpu=fpv4-sp-d16 \
+                          -fmessage-length=0 \
+                          -ffunction-sections \
+                          -fdata-sections
+# C standard
+TOOLCHAIN_CSTANDARD := -std=gnu99
+
+# Linker script location
+LDSCRIPT := source/STM32F429VI_FLASH.ld
+
+# Linker flags
+LDFLAGS      := -T$(LDSCRIPT) \
+                -static \
+                -Wl,-cref,-u,Reset_Handler \
+                -Wl,-Map=$(BIN_DIR)/$(PROJECT_NAME).map \
+                -Wl,--gc-sections \
+                -Wl,--defsym=malloc_getpagesize_P=0x80 \
+                -Wl,--start-group -lc -lm -Wl,--end-group \
+                --specs=nano.specs
+
+# Compiler flags
+CFLAGS := -Wall \
+          # -Wextra \
+          # -Wfatal-errors \
+          # -Wpacked \
+          # -Winline \
+          # -Wfloat-equal \
+          # -Wconversion \
+          # -Wlogical-op \
+          # -Wpointer-arith \
+          # -Wdisabled-optimization \
+          # -Wno-unused-parameter
+
+# Build type modifiers
+CFLAGS_DEBUG := -ggdb \
+                -g3 \
+                -Og
+LDFLAGS_DEBUG := --specs=rdimon.specs \
+                 -lrdimon \
+                 -Og
+CFLAGS_RELEASE  := -Os
+LDFLAGS_RELEASE := --specs=nosys.specs
+
+CFLAGS  += $(CFLAGS_$(call toupper,$(BUILD_TYPE))) \
+           $(TOOLCHAIN_COMMON_FLAGS) \
+           $(TOOLCHAIN_CSTANDARD)
+
+LDFLAGS += $(LDFLAGS_$(call toupper,$(BUILD_TYPE))) \
+           $(TOOLCHAIN_COMMON_FLAGS)
+
+# Build artifacts
+TARGET_ELF   := $(BIN_DIR)/$(PROJECT_NAME).elf
+TARGET_BIN   := $(BIN_DIR)/$(PROJECT_NAME).bin
+TARGET_HEX   := $(BIN_DIR)/$(PROJECT_NAME).hex
